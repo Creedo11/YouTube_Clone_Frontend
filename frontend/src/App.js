d654ebb00997db80d39d8ea6dda7fcc76b843ce7
Creedo11 <keegkree@gmail.com>
2022-10-24 22:15:38 -0400

    removed private router from HomePage component in the App.js file + commented out existing fetchCars logc within HomePage.js + wrote a function to display a hard coded embedded youtube video withn HomePage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,21 +12,14 @@ import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 
 // Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
+// import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import useAuth from "../../hooks/useAuth";
+import React, { useState } from "react";
+// import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
 
@@ -8,33 +7,49 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  
+//   const [user, token] = useAuth();
+//   const [cars, setCars] = useState([]);
 
-  useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
-  }, [token]);
+//   useEffect(() => {
+//     const fetchCars = async () => {
+//       try {
+//         let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+//           headers: {
+//             Authorization: "Bearer " + token,
+//           },
+//         });
+//         setCars(response.data);
+//       } catch (error) {
+//         console.log(error.response.data);
+//       }
+//     };
+//     fetchCars();
+//   }, [token]);
+//   return (
+//     <div className="container">
+//       <h1>Home Page for {user.username}!</h1>
+//       {cars &&
+//         cars.map((car) => (
+//           <p key={car.id}>
+//             {car.year} {car.model} {car.make}
+//           </p>
+//         ))}
+//     </div>
+//   );
+// };
+
+  const [video, setVideo] = useState([]);
   return (
-    <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
+    <div>
+      <iframe 
+        id="ytplayer" 
+        type="text/html" 
+        width="640" 
+        height="360"
+        src="https://www.youtube.com/embed/g4Hbz2jLxvQ?autoplay=1&origin=http://example.com"
+        frameborder="0">
+      </iframe>
     </div>
   );
 };
